15a840918141f6f47fd51a24777394f13d6aa91e
Tarak Shah <tarak@hrdag.org>
2018-12-04 15:52:25 -0800

    use 'sub' instead of 'replace', related to #2

--- tests/jsweave_test.py
+++ tests/jsweave_test.py
@@ -1,21 +1,20 @@
 import imp
 jsweave = imp.load_source("jsweave", "../bin/jsweave")
 from jsweave import *
-from subprocess import call
 
-def test_replace():
+def test_sub():
     TeX = "\\jsonsub[40]{datacrime}"
     JS = {'datacrime':'30'}
-    replaced_str = replace(TeX, JS)
+    subd_str = sub(TeX, JS)
     expected_str = '30'
-    assert replaced_str == expected_str
+    assert subd_str == expected_str
 
-def test_replace_percent():
+def test_sub_percent():
     TeX = "\\jsonsub[40\%]{datacrime}"
     JS = {'datacrime':'30%'}
-    replaced_str = replace(TeX, JS)
+    subd_str = sub(TeX, JS)
     expected_str = '30\%'
-    assert replaced_str == expected_str
+    assert subd_str == expected_str
 
 def test_update():
     TeX = "\\jsonsub[40\%]{datacrime}"
@@ -31,10 +30,10 @@ def test_check():
     expected_result = [(1, "40%", "30%")]
     assert check_output == expected_result
 
-def test_acceptance_replace():
+def test_acceptance_sub():
     before_tex = getTeX("data/before.tex")
     fake_json = getJson("data/fake-json.json")
-    res = replace(before_tex, fake_json)
+    res = sub(before_tex, fake_json)
     assert res == getTeX("data/expected.tex")    
 
 def test_acceptance_update():
@@ -46,9 +45,8 @@ def test_acceptance_update():
 def test_acceptance_check():
     before_tex = getTeX("data/before.tex")
     updated_json = getJson("data/updated-json.json")
-    check_result = check(Tex, JS)
-    expected_result = 
-        [
+    check_result = check(before_tex, updated_json)
+    expected_result = [
             (11, "20", "77"),
             (13, "204,337.73", "197,283.11"),
             (15, "55%", "1%")
