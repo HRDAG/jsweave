e8635b27b039b4aa1efc9f5558e59625ce051ac0
Tarak Shah <tarakc02@users.noreply.github.com>
2018-12-10 12:02:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Author: Daniel Manrique-Vallier
-# Maintainer(s): Tarak Shah, Patrick Ball, Daniel Manrique-Vallier
+# Author: Daniel Manrique-Vallier, Shemika Lamare, Tarak Shah
+# Maintainer(s): Tarak Shah, Patrick Ball, Daniel Manrique-Vallier, Shemika Lamare
 #
 # License: (c) Daniel Manrique-Vallier 2018, GPL v2 or newer
 
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='jsweave',
-    version='0.1',
+    version='0.1-dev',
     description='weave values by name from json name:value into LaTeX',
     url='https://github.com/HRDAG/jsweave',
     author='Daniel Manrique-Vallier',
