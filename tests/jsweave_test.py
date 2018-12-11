45d2c1f58c18eaa8cad50e16f0b26cf7aa5f3173
Tarak Shah <tarak@hrdag.org>
2018-12-11 12:09:13 -0800

    varnames in lower-case

--- tests/jsweave_test.py
+++ tests/jsweave_test.py
@@ -4,66 +4,66 @@ from jsweave import sub, check, update, get_tex, get_json
 
 
 def test_sub():
-    TeX = r"\jsonsub[40]{datacrime}"
-    JS = {'datacrime': '30'}
-    subd_str = sub(TeX, JS)
+    tex = r"\jsonsub[40]{datacrime}"
+    js = {'datacrime': '30'}
+    subd_str = sub(tex, js)
     expected_str = '30'
     assert subd_str == expected_str
 
 def test_sub_plus_text():
-    TeX = r"a value of \jsonsub[40]{datacrime} is too high"
-    JS = {'datacrime': '30'}
-    subd_str = sub(TeX, JS)
+    tex = r"a value of \jsonsub[40]{datacrime} is too high"
+    js = {'datacrime': '30'}
+    subd_str = sub(tex, js)
     expected_str = 'a value of 30 is too high'
     assert subd_str == expected_str
 
 def test_sub_no_default():
-    TeX = "\\jsonsub{datacrime} is too high"
-    JS = {'datacrime': '30'}
-    subd_str = sub(TeX, JS)
+    tex = "\\jsonsub{datacrime} is too high"
+    js = {'datacrime': '30'}
+    subd_str = sub(tex, js)
     expected_str = '30 is too high'
     assert subd_str == expected_str
 
 def test_sub_percent():
-    TeX = r"\jsonsub[40\%]{datacrime}"
-    JS = {'datacrime': '30%'}
-    subd_str = sub(TeX, JS)
+    tex = r"\jsonsub[40\%]{datacrime}"
+    js = {'datacrime': '30%'}
+    subd_str = sub(tex, js)
     expected_str = '30\\%'
     assert subd_str == expected_str
 
 def test_sub_integer():
-    TeX = r"\jsonsub[40]{datacrime}"
-    JS = {'datacrime': 30}
-    subd_str = sub(TeX, JS)
+    tex = r"\jsonsub[40]{datacrime}"
+    js = {'datacrime': 30}
+    subd_str = sub(tex, js)
     expected_str = '30'
     assert subd_str == expected_str
 
 # def test_sub_escaped_percent():
-#     TeX = r"\\jsonsub[40\%]{datacrime}"
-#     JS = {'datacrime': r'30\%'}
-#     subd_str = sub(TeX, JS)
+#     tex = r"\\jsonsub[40\%]{datacrime}"
+#     js = {'datacrime': r'30\%'}
+#     subd_str = sub(tex, js)
 #     expected_str = r'30\\%'
 #     assert subd_str == expected_str
 
 def test_update():
-    TeX = r"\jsonsub[40\%]{datacrime}"
-    JS = {'datacrime': '30%'}
-    updated_str = update(TeX, JS)
+    tex = r"\jsonsub[40\%]{datacrime}"
+    js = {'datacrime': '30%'}
+    updated_str = update(tex, js)
     expected_str = r'\jsonsub[30\%]{datacrime}'
     assert updated_str == expected_str
 
 def test_check():
-    TeX = r"\jsonsub[40\%]{datacrime}"
-    JS = {'datacrime':'30%'}
-    check_output = check(TeX, JS)
+    tex = r"\jsonsub[40\%]{datacrime}"
+    js = {'datacrime':'30%'}
+    check_output = check(tex, js)
     expected_result = [(1, "40\\%", "30\\%")]
     assert check_output == expected_result
 
 # if no change, then don't report anything
 def test_check2():
-    TeX = r"\jsonsub[40\%]{datacrime}"
-    JS = {'datacrime':'40%'}
-    check_output = check(TeX, JS)
+    tex = r"\jsonsub[40\%]{datacrime}"
+    js = {'datacrime':'40%'}
+    check_output = check(tex, js)
     expected_result = []
     assert check_output == expected_result
 
