99d2915b550c5fcef6a0db18d1f36dde7628b261
Tarak Shah <tarak@hrdag.org>
2018-12-11 12:04:08 -0800

    no camelCase etc. functions

--- tests/jsweave_test.py
+++ tests/jsweave_test.py
@@ -1,6 +1,6 @@
 import imp
 jsweave = imp.load_source("jsweave", "../bin/jsweave")
-from jsweave import sub, check, update, getTeX, getJson
+from jsweave import sub, check, update, get_tex, get_json
 
 
 def test_sub():
@@ -68,20 +68,20 @@ def test_check2():
     assert check_output == expected_result
 
 def test_acceptance_sub():
-    before_tex = getTeX("data/before.tex")
-    fake_json = getJson("data/fake-json.json")
+    before_tex = get_tex("data/before.tex")
+    fake_json = get_json("data/fake-json.json")
     res = sub(before_tex, fake_json)
-    assert res == getTeX("data/expected.tex")
+    assert res == get_tex("data/expected.tex")
 
 def test_acceptance_update():
-    before_tex = getTeX("data/before.tex")
-    updated_json = getJson("data/updated-json.json")
+    before_tex = get_tex("data/before.tex")
+    updated_json = get_json("data/updated-json.json")
     result = update(before_tex, updated_json)
-    assert result == getTeX("data/expected-updated.tex")
+    assert result == get_tex("data/expected-updated.tex")
 
 def test_acceptance_check():
-    before_tex = getTeX("data/before.tex")
-    updated_json = getJson("data/updated-json.json")
+    before_tex = get_tex("data/before.tex")
+    updated_json = get_json("data/updated-json.json")
     check_result = check(before_tex, updated_json)
     expected_result = [
             (11, "20", "77"),
